4ff23f11cdab2cf5665280d65d119b47323cfb1f
Elena Spradlin <ekspradlin@gmail.com>
2023-05-29 12:43:59 -0400

    Updates

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "The Destabilizer",
+            power: "Wrecks your day"
+        },
+        {
+            id: 2,
+            name: "Silent Mode",
+            power: "Stonewalling"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
